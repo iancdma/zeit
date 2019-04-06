d2cc4e95653f466cf3038666a81c9e9d8b5456e1
Ian Carlo Alincastre <dev.rigel.bb@gmail.com>
2019-04-06 16:49:55 +0800

    Changed homepage content

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This Zeit Test App was made by iancdma.
           </p>
           <a
             className="App-link"
